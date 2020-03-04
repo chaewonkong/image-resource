23206349c232ea09d11cb16ffb7add744ef3f415
Kong Chae Won <chaewonkong@gmail.com>
2020-03-04 11:53:36 +0900

    chage deepcopy img

---
+++ 1.js
@@ -0,0 +1,12 @@
+function cloneObject(obj) {
+  var clone = {};
+  for (var key in obj) {
+    if (typeof obj[key] == "object" && obj[key] != null) {
+      clone[key] = cloneObject(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+
+  return clone;
+}
